dc8d6f40c79bfc5e6a7fa623d80a8eaa068f068c
PALENGE <palenge@gmail.com>
2020-11-12 23:35:26 +0100

    planets

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void Update()
+    {
+        transform.rotation = cam.transform.rotation;
+    }
+}
